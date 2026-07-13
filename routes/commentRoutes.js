959a704028306852bd74122eb84c1e7e3dafd804
sononevlogsandhya-cyber <sononevlogsandhya@gmail.com>
2026-07-13 17:24:26 +0530

    Delete routes directory

--- routes/commentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { protect } = require("../middleware/authMiddleware");
-const { editComment, deleteComment } = require("../controllers/commentController");
-
-router.put("/:id", protect, editComment);
-router.delete("/:id", protect, deleteComment);
-
-module.exports = router;
